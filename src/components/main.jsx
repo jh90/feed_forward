0c8f65d4299c862d7f764f1e5b269a6e52c9255b
Jake Hamilton <hamiltonjm12@gmail.com>
2016-09-22 16:51:47 -0400

    Start building main.jsx

--- src/components/main.jsx
+++ src/components/main.jsx
@@ -3,5 +3,37 @@ index view: allposts
 goto alias: text input, on submit passes value to getIdByAlias, passes its return to redirect
   ^ separate component?
 getIdByAlias: searches for uid matching alias passed as parameter
-redirect: takes URL or keyword (?) and redirects appropriately - passed as prop to LoginForm and
-  RegisterForm
+
+import React from 'react';
+import {Link} from 'react-router';
+
+export default class Main extends React.Component {
+  constructor () {
+    super();
+    this.state = {
+      loggedIn: false,
+    };
+  }
+
+  componentWillMount () {
+    setTimeout(() => {
+      firebase.auth().onAuthStateChanged((user) => {
+        this.setState({loggedIn: (user !== null),});
+      });
+    }, 200);
+  }
+
+  signOut () {
+    firebase.auth()
+      .signOut()
+      .then(() => {
+        console.log('dey gon');
+      });
+  }
+
+  render () {
+    return (
+
+    );
+  }
+}
