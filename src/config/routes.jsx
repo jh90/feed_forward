7a5b326d3b7de8c50be0a8f2000e267e6f26dcd7
Jake Hamilton <hamiltonjm12@gmail.com>
2016-09-22 16:27:47 -0400

    Build routes.jsx, index.js, index.html

--- src/config/routes.jsx
+++ src/config/routes.jsx
@@ -1,18 +1,24 @@
-Route / Main
-  IndexRoute FeedView
-  Route users
-    Route all UserList
-    Route :id FeedView
-  /Route
-  Route login LoginForm
-  Route register RegisterForm
-/Route
+import React from 'react';
+import {Router, Route, IndexRoute, hashHistory} from 'react-router';
 
-==
-feedforward.io/ MAIN
-  [index] FEEDVIEW:ALL
-  users/
-    all USERLIST
-    :id FEEDVIEW:UID
-  login LOGINFORM
-  register REGISTERFORM
+import Main from '../components/main.jsx';
+import Feed from '../components/feed.jsx';
+import UserList from '../components/user_list.jsx';
+import LoginForm from '../components/login_form.jsx';
+import RegisterForm from '../components/register_form.jsx';
+
+const Routes = () => {
+  return (
+    <Router history={hashHistory}>
+      <Route path='/' component={Main} >
+        <IndexRoute component={Feed} />
+        <Route path='users' >
+          <Route path='all' component={UserList} />
+          <Route path=':id' component={Feed} />
+        </Route>
+        <Route path='login' component={LoginForm} />
+        <Route path='register' component={RegisterForm} />
+      </Route>
+    </Router>
+  );
+};
---
+++ src/index.js
@@ -0,0 +1,6 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+import Routes from './config/routes.jsx';
+
+ReactDOM.render(<Routes />, document.querySelector('#root'));
