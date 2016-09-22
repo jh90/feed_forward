799f53facc32b0f46dc602c3fd6cc07ea735bce3
Jake Hamilton <hamiltonjm12@gmail.com>
2016-09-22 13:21:52 -0400

    Install NPM packages, build webpack.config.js, create comment.jsx and new_post_form.jsx, rename some component files

---
+++ src/components/post.jsx
@@ -0,0 +1,8 @@
+state.modalOpen default false
+render: displays link [image/text/headline-?]; commentary if it exists; date of post; comment button;
+  header, onClick calls modalOpen; if .props.uid == firebase.currentUser, button onClick calls
+  removePost
+modalOpen: sets modalOpen to true (triggers rerender)
+modalClose: sets modalOpen to false, passed to PostViewModal as prop
+removePost: makes delete request to /posts/pid
+child component: PostViewModal
