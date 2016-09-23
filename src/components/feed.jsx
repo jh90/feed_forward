a8421f173525f666b4a0515e3a524b455b9201fd
Jake Hamilton <hamiltonjm12@gmail.com>
2016-09-23 13:25:38 -0400

    Debug GET method in feed, build out v1 of userlist

--- src/components/feed.jsx
+++ src/components/feed.jsx
@@ -1,11 +1,3 @@
-accepts but does not require uid string param from route
-state defaults: .isMain true, .orderByVotes false, posts []
-didMount: if params exist sets .isMain=false; calls getPosts
-getPosts: if .isMain, gets all in database/posts, else gets all whose uid matches
-  param; if .orderByVotes, sorts by votecount; sets .posts
-render: creates list; iterates through .posts and passes them as props to PostDisplay components;
-  creates button, onClick sets .orderByVotes=true and calls getPosts
-
 import React from 'react';
 import request from 'superagent';
 
@@ -41,10 +33,10 @@ export default class Feed extends React.Component {
     const allPosts;
     request.get(url).then((response) => {
       allPosts = response.body;
+      const postList = Array.from(allPosts);
+      postList.reverse();
+      return postList;
     });
-    const postList = Array.from(allPosts);
-    postList.reverse();
-    return postList;
   }
 
   sortByVotes (postArray) {
--- src/components/user_list.jsx
+++ src/components/user_list.jsx
@@ -1,2 +1,21 @@
 dummy component
-render: displays all in db/users by alias as Link to:/users/uid + [thumbnail of?] most recent post
+displays all in db/users by alias as Link to:/users/uid + [thumbnail of?] most recent post
+
+import React from 'react';
+import {Link} from 'react-router';
+import request from 'superagent';
+
+const UserList = () => {
+  return (
+    <div>
+      {
+        const url = 'https://feedforward-968b7.firebaseio.com/users';
+        request.get(url).then((response) => {
+          response.map((user) => {
+            <Link to=`/users/${user.id}` addClass='user-link'>{user.alias}</Link>
+          });
+        })
+      }
+    </div>
+  );
+}
